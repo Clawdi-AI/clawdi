d3448464f5288ccd0067690cdf7ea2857e740a13
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2026-08-30 12:38:22 +0000

    fix(desktop): use the bundled V8 snapshot

--- apps/desktop/scripts/after-pack.mjs
+++ apps/desktop/scripts/after-pack.mjs
@@ -22,7 +22,7 @@ export default async function afterPack(context) {
 		[FuseV1Options.EnableNodeCliInspectArguments]: false,
 		[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
 		[FuseV1Options.OnlyLoadAppFromAsar]: true,
-		[FuseV1Options.LoadBrowserProcessSpecificV8Snapshot]: true,
+		[FuseV1Options.LoadBrowserProcessSpecificV8Snapshot]: false,
 		[FuseV1Options.GrantFileProtocolExtraPrivileges]: false,
 		[FuseV1Options.WasmTrapHandlers]: true,
 	});
